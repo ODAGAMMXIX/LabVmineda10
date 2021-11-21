2df1e4893f5c0fc1f3d781dd4244dcd8b9969bcb
Odaga <fabio.odaguiri@fatec.sp.gov.br>
2021-11-21 15:56:34 -0300

    ccccccccc

--- src/main/java/br/gov/sp/fatec/veterinario/service/SegurancaServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/veterinario/service/SegurancaServiceImpl.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
